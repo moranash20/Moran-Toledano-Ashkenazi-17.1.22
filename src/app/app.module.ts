a42aee03d15ac133ce93d0e8b7af7075671e674c
מורן אשכנזי - טולידנו <moranash20@gmail.com>
2022-01-31 09:04:07 +0200

    last changes

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from "@angular/common/http";
+import {HeaderComponent} from './core/components/header/header.component';
+import {SharedModule} from "./shared/shared.module";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    SharedModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
